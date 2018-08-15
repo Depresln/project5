9335ac70cc3b43b6299d6853aeea4555ec11596b
Exal <depresles.nicolas@gmail.com>
2018-08-15 14:58:46 +0200

    Added style.css and worked on post_index css

--- src/View/Post/post_index.php
+++ src/View/Post/post_index.php
@@ -5,9 +5,19 @@ ob_start();
 
     // Navbar
     require '../src/View/navbar.php';
+?>
 
-    echo '<br /><br /><br /><br /><br /><h2>Post List</h2>';
+    <br /><br /><br /><br /><br />
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h2>Liste des posts</h2>
+                <hr class="star-primary -align-left">
+            </div>
+        </div>
+    </div>
 
+<?php
     // Message for post deletion successful
     if(isset($_SESSION['pseudo'])){
         if($_SESSION['is_admin'] == TRUE){
@@ -41,27 +51,63 @@ ob_start();
     // Post index
     /** @var \App\Entity\Post $post */
     foreach ($postList as $post) {
-        echo "<a href='?page=post.show&id=" . $post->getId() . "'>" . $post->getTitle() . "</a> ";
-        echo $post->getDate() . "<br />";
-        echo $post->getChapo() . "<br />";
-        if(isset($_SESSION['pseudo'])) {
-            if ($_SESSION['is_admin'] == TRUE) {
-                echo "<a href='?page=post.delete&id=" . $post->getId() . "'>Supprimer le post</a><br /><br />";
-            }
-        }
+        ?>
+        <div class="container fa-border">
+            <div class="row">
+                <div class="col-lg-11 col-lg-offset-1">
+                    <h2><?php echo "<a href='?page=post.show&id=" . $post->getId() . "'>" . $post->getTitle() . "</a> "; ?></h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-lg-offset-1">
+                    <p>
+                        <?php echo $post->getChapo(); ?>
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-lg-offset-3">
+                    <ul class="list-inline">
+                        <li>
+                            <?php echo $post->getDate(); ?>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-lg-2">
+                    <ul class="list-inline">
+                        <li>
+                            <?php
+                            if(isset($_SESSION['pseudo'])) {
+                                if ($_SESSION['is_admin'] == TRUE) {
+                                    echo "<a href='?page=post.delete&id=" . $post->getId() . "'>Supprimer le post</a>";
+                                }
+                            }
+                            ?>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <br />
+        <?php
     }
 
     // Add a post
     if(isset($_SESSION['pseudo'])){
         if($_SESSION['is_admin'] == TRUE){
-            echo "<a href='?page=post.create'>Ajouter un post</a><br /><br />";
-        }
-    }
-
-    // Espace admin
-    if(isset($_SESSION['pseudo'])){
-        if($_SESSION['is_admin'] == TRUE){
-            echo "<a href='?page=post.administration'>Accéder à l'espace administrateur</a><br /><br />";
+            ?>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-offset-3 col-lg-3">
+                        <?php echo "<a href='?page=post.create' class='btn btn-lg btn-success'>Ajouter un post</a>"; ?>
+                    </div>
+                    <div class="col-lg-3">
+                        <?php echo "<a href='?page=post.administration' class='btn btn-lg btn-success'>Accéder à l'espace administrateur</a>"; ?>
+                    </div>
+                </div>
+            </div>
+            <br />
+            <?php
         }
     }
 
@@ -70,3 +116,4 @@ ob_start();
 
 $content = ob_get_clean();
 include('..\src\View\template.php');
+?>
